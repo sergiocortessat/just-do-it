d4372ea19b684b1a3b17e5671ef3301dc967c809
Sergio Cortes <sergiocortessat@gmail.com>
2021-10-15 14:07:57 +0200

    Add minor changes

--- src/Basket.js
+++ src/Basket.js
@@ -15,7 +15,6 @@ const Basket = ({products = [], onDelete = Function, totalSum= 0}) => {
 
     const headers = products[0] && Object.keys(products[0]);
     const content = products[0] && Object.values(products);
-    console.log(content);
     return (
         <div>
         <table>
